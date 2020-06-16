10c169d2f0e9248d09e75baf03522799df7688f4
Jieuniii <yjeka@ajou.ac.kr>
2020-06-16 15:40:37 +0900

    Shadow Cooking

--- hci_project_2/app/src/main/java/com/sklee/asd/course13Activity.java
+++ hci_project_2/app/src/main/java/com/sklee/asd/course13Activity.java
@@ -2,7 +2,9 @@ package com.sklee.asd;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,12 +12,42 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Locale;
+
 public class course13Activity extends AppCompatActivity {
+    private TextToSpeech textToSpeech;
+    private Button btnEnter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course13);
+
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = textToSpeech.setLanguage(Locale.KOREA);
+                    //언어 데이터가 없거나 혹은 언어가 지원하지 않으면
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Toast.makeText(course13Activity.this, "이 언어는 지원하지 않습니다.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        btnEnter.setEnabled(true);
+                        //음성 톤
+                        textToSpeech.setPitch(0.7f);
+                        //읽는 속도
+                        textToSpeech.setSpeechRate(1.2f);
+                    }
+                }
+            }
+        });
+        btnEnter = (Button) findViewById(R.id.btn_ent);
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Speech();
+            }
+        });
 
         Button pre = (Button) findViewById(R.id.pre);
         pre.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +69,23 @@ public class course13Activity extends AppCompatActivity {
                 startActivityForResult(intent, 114);
             }
         });
+    }
+
+    private void Speech() {
+        String text = "마지막으로 후춧가루를 톡톡 뿌리면 완성~";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        else
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+
+    }
+
+    protected void onStop() {
+        super.onStop();
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
     }
 
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
